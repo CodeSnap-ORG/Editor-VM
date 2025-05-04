73bfc8ca26cae23b82fe08953f18e730cfe47106
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-04 15:55:46 +0100

    Update virtual-machine.js

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -428,57 +428,69 @@ class VirtualMachine extends EventEmitter {
     }
 
     /**
-     * Load a Scratch project from a .sb, .sb2, .sb3 or JSON string.
-     * @param {string | object} input A JSON string, object, or ArrayBuffer representing the project to load.
+     * Load a Scratch project from a .sb, .sb2, .sb3 or json string.
+     * @param {string | object} input A json string, object, or ArrayBuffer representing the project to load.
      * @return {!Promise} Promise that resolves after targets are installed.
      */
-    async loadProject () {
-        try {
-            // Stop the current VM project
-            await this.props.vm.quit();
+    loadProject (input) {
+        if (typeof input === 'object' && !(input instanceof ArrayBuffer) &&
+          !ArrayBuffer.isView(input)) {
+            // If the input is an object and not any ArrayBuffer
+            // or an ArrayBuffer view (this includes all typed arrays and DataViews)
+            // turn the object into a JSON string, because we suspect
+            // this is a project.json as an object
+            // validate expects a string or buffer as input
+            // TODO not sure if we need to check that it also isn't a data view
+            input = JSON.stringify(input);
+        }
 
-            let input = this.props.projectData;
-
-            // Check if the projectId is a number or numeric string
-            if (
-                typeof this.props.projectId === 'number' ||
-                (typeof this.props.projectId === 'string' && /^\d+$/.test(this.props.projectId))
-            ) {
-                try {
-                    const response = await fetch(`https://block-compiler-codesnap.onrender.com/projects/${this.props.projectId}`);
-
-                    if (!response.ok) {
-                        throw new Error(`Failed to fetch project: ${response.statusText}`);
-                    }
-
-                    // Adjust based on your project format: .sb3 files should use arrayBuffer
-                    input = await response.arrayBuffer();
-                } catch (e) {
-                    console.error('Error fetching project:', e);
-                    this.props.onError(e);
-                    return;
+        const validationPromise = new Promise((resolve, reject) => {
+            const validate = require('scratch-parser');
+            // The second argument of false below indicates to the validator that the
+            // input should be parsed/validated as an entire project (and not a single sprite)
+            validate(input, false, (error, res) => {
+                if (error) {
+                    return reject(error);
                 }
-            }
+                resolve(res);
+            });
+        })
+            .catch(error => {
+                const {SB1File, ValidationError} = require('scratch-sb1-converter');
 
-            // Load the project into the VM
-            await this.props.vm.loadProject(input);
-
-            // Notify that the project was successfully loaded
-            this.props.onLoadedProject(this.props.loadingState, this.props.canSave);
-
-            setTimeout(() => {
-                this.props.onSetProjectUnchanged();
+                try {
+                    const sb1 = new SB1File(input);
+                    const json = sb1.json;
+                    json.projectVersion = 2;
+                    return Promise.resolve([json, sb1.zip]);
+                } catch (sb1Error) {
+                    if (
+                        sb1Error instanceof ValidationError ||
+                        `${sb1Error}`.includes('Non-ascii character in FixedAsciiString')
+                    ) {
+                        // The input does not validate as a Scratch 1 file.
+                    } else {
+                        // The project appears to be a Scratch 1 file but it
+                        // could not be successfully translated into a Scratch 2
+                        // project.
+                        return Promise.reject(sb1Error);
+                    }
+                }
+                // Throw original error since the input does not appear to be
+                // an SB1File.
+                return Promise.reject(error);
             });
 
-            if (!this.props.isStarted) {
-                setTimeout(() => {
-                    this.props.vm.renderer.draw();
-                });
-            }
-        } catch (e) {
-            console.error('Error loading project:', e);
-            this.props.onError(e);
-        }
+        return validationPromise
+            .then(validatedInput => this.deserializeProject(validatedInput[0], validatedInput[1]))
+            .then(() => this.runtime.handleProjectLoaded())
+            .catch(error => {
+                // Intentionally rejecting here (want errors to be handled by caller)
+                if (Object.prototype.hasOwnProperty.call(error, 'validationError')) {
+                    return Promise.reject(JSON.stringify(error));
+                }
+                return Promise.reject(error);
+            });
     }
 
     /**
